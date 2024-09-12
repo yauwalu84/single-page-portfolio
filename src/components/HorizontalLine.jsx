c53ea02a3479fc9d42df7244e7acf9cb27276e7c
yahaya.auwalu@azubiafrica.org <yahaya.auwalu@azubiafrica.org>
2024-09-12 10:35:42 +0100

    first commit

---
+++ src/components/HorizontalLine.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import PropTypes from "prop-types";
+
+const HorizontalLine = ({ className }) => {
+  return <hr className={`h-[1px] w-full bg-white ${className}`} />;
+};
+
+HorizontalLine.propTypes = {
+  className: PropTypes.string,
+};
+
+export default HorizontalLine;
